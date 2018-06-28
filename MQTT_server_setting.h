c95af6031aa1dc16f31a235afacba11420fae4c8
Osamu Koizumi <Osamu.Koizumi@arm.com>
2018-06-28 12:31:37 +0900

    Adapt to Google IoT Core. Initial version.

--- MQTT_server_setting.h
+++ MQTT_server_setting.h
@@ -1,13 +1,13 @@
 #ifndef __MQTT_SERVER_SETTING_H__
 #define __MQTT_SERVER_SETTING_H__
 
-const char MQTT_SERVER_HOST_NAME[] = "<< REPLACE_HERE >>";
-const char MQTT_CLIENT_ID[] = "<< REPLACE_HERE >>";
-const char MQTT_USERNAME[] = "<< REPLACE_HERE >>";
-const char MQTT_PASSWORD[] = "<< REPLACE_HERE >>";
-const char MQTT_TOPIC_PUB[] = "<< REPLACE_HERE >>";
-const char MQTT_TOPIC_SUB[] = "<< REPLACE_HERE >>";
 
+const char* GOOGLE_PROJECT_ID = "<< YOUR PROJECT ID >>";
+const char* GOOGLE_REGION = "<< YOUR REGION >>";
+const char* GOOGLE_REGISTRY = "<< YOUR REGISTRY ID >>";
+const char* GOOGLE_DEVICE_ID = "<< YOUR DEVICE ID >>";
+
+const char MQTT_SERVER_HOST_NAME[] = "mqtt.googleapis.com";
 
 const int MQTT_SERVER_PORT = 8883;
 
@@ -17,7 +17,100 @@ const int MQTT_SERVER_PORT = 8883;
  * ...
  * "-----END CERTIFICATE-----\n";
  */
-const char* SSL_CA_PEM = NULL;
+// Obtained and picked up from https://pki.google.com/roots.pem
+const char* SSL_CA_PEM =
+"# Operating CA: GlobalSign\n"
+"# Issuer: CN=GlobalSign Root CA O=GlobalSign nv-sa OU=Root CA\n"
+"# Subject: CN=GlobalSign Root CA O=GlobalSign nv-sa OU=Root CA\n"
+"# Label: \"GlobalSign Root CA\"\n"
+"# Serial: 4835703278459707669005204\n"
+"# MD5 Fingerprint: 3e:45:52:15:09:51:92:e1:b7:5d:37:9f:b1:87:29:8a\n"
+"# SHA1 Fingerprint: b1:bc:96:8b:d4:f4:9d:62:2a:a8:9a:81:f2:15:01:52:a4:1d:82:9c\n"
+"# SHA256 Fingerprint: eb:d4:10:40:e4:bb:3e:c7:42:c9:e3:81:d3:1e:f2:a4:1a:48:b6:68:5c:96:e7:ce:f3:c1:df:6c:d4:33:1c:99\n"
+"-----BEGIN CERTIFICATE-----\n"
+"MIIDdTCCAl2gAwIBAgILBAAAAAABFUtaw5QwDQYJKoZIhvcNAQEFBQAwVzELMAkG\n"
+"A1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jv\n"
+"b3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw05ODA5MDExMjAw\n"
+"MDBaFw0yODAxMjgxMjAwMDBaMFcxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i\n"
+"YWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290IENBMRswGQYDVQQDExJHbG9iYWxT\n"
+"aWduIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDaDuaZ\n"
+"jc6j40+Kfvvxi4Mla+pIH/EqsLmVEQS98GPR4mdmzxzdzxtIK+6NiY6arymAZavp\n"
+"xy0Sy6scTHAHoT0KMM0VjU/43dSMUBUc71DuxC73/OlS8pF94G3VNTCOXkNz8kHp\n"
+"1Wrjsok6Vjk4bwY8iGlbKk3Fp1S4bInMm/k8yuX9ifUSPJJ4ltbcdG6TRGHRjcdG\n"
+"snUOhugZitVtbNV4FpWi6cgKOOvyJBNPc1STE4U6G7weNLWLBYy5d4ux2x8gkasJ\n"
+"U26Qzns3dLlwR5EiUWMWea6xrkEmCMgZK9FGqkjWZCrXgzT/LCrBbBlDSgeF59N8\n"
+"9iFo7+ryUp9/k5DPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8E\n"
+"BTADAQH/MB0GA1UdDgQWBBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0B\n"
+"AQUFAAOCAQEA1nPnfE920I2/7LqivjTFKDK1fPxsnCwrvQmeU79rXqoRSLblCKOz\n"
+"yj1hTdNGCbM+w6DjY1Ub8rrvrTnhQ7k4o+YviiY776BQVvnGCv04zcQLcFGUl5gE\n"
+"38NflNUVyRRBnMRddWQVDf9VMOyGj/8N7yy5Y0b2qvzfvGn9LhJIZJrglfCm7ymP\n"
+"AbEVtQwdpf5pLGkkeB6zpxxxYu7KyJesF12KwvhHhm4qxFYxldBniYUr+WymXUad\n"
+"DKqC5JlR3XC321Y9YeRq4VzW9v493kHMB65jUr9TU/Qr6cf9tveCX4XSQRjbgbME\n"
+"HMUfpIBvFSDJ3gyICh3WZlXi/EjJKSZp4A==\n"
+"-----END CERTIFICATE-----\n"
+"\n"
+"# Operating CA: GlobalSign\n"
+"# Issuer: CN=GlobalSign O=GlobalSign OU=GlobalSign Root CA - R2\n"
+"# Subject: CN=GlobalSign O=GlobalSign OU=GlobalSign Root CA - R2\n"
+"# Label: \"GlobalSign Root CA - R2\"\n"
+"# Serial: 4835703278459682885658125\n"
+"# MD5 Fingerprint: 94:14:77:7e:3e:5e:fd:8f:30:bd:41:b0:cf:e7:d0:30\n"
+"# SHA1 Fingerprint: 75:e0:ab:b6:13:85:12:27:1c:04:f8:5f:dd:de:38:e4:b7:24:2e:fe\n"
+"# SHA256 Fingerprint: ca:42:dd:41:74:5f:d0:b8:1e:b9:02:36:2c:f9:d8:bf:71:9d:a1:bd:1b:1e:fc:94:6f:5b:4c:99:f4:2c:1b:9e\n"
+"-----BEGIN CERTIFICATE-----\n"
+"MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G\n"
+"A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp\n"
+"Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1\n"
+"MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG\n"
+"A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n"
+"hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL\n"
+"v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8\n"
+"eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq\n"
+"tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n"
+"C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa\n"
+"zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB\n"
+"mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH\n"
+"V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n\n"
+"bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG\n"
+"3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs\n"
+"J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO\n"
+"291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS\n"
+"ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n"
+"AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7\n"
+"TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==\n"
+"-----END CERTIFICATE-----\n"
+"\n"
+"# Operating CA: GlobalSign\n"
+"# Issuer: CN=GlobalSign O=GlobalSign OU=GlobalSign Root CA - R3\n"
+"# Subject: CN=GlobalSign O=GlobalSign OU=GlobalSign Root CA - R3\n"
+"# Label: \"GlobalSign Root CA - R3\"\n"
+"# Serial: 4835703278459759426209954\n"
+"# MD5 Fingerprint: c5:df:b8:49:ca:05:13:55:ee:2d:ba:1a:c3:3e:b0:28\n"
+"# SHA1 Fingerprint: d6:9b:56:11:48:f0:1c:77:c5:45:78:c1:09:26:df:5b:85:69:76:ad\n"
+"# SHA256 Fingerprint: cb:b5:22:d7:b7:f1:27:ad:6a:01:13:86:5b:df:1c:d4:10:2e:7d:07:59:af:63:5a:7c:f4:72:0d:c9:63:c5:3b\n"
+"-----BEGIN CERTIFICATE-----\n"
+"MIIDXzCCAkegAwIBAgILBAAAAAABIVhTCKIwDQYJKoZIhvcNAQELBQAwTDEgMB4G\n"
+"A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjMxEzARBgNVBAoTCkdsb2JhbFNp\n"
+"Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDkwMzE4MTAwMDAwWhcNMjkwMzE4\n"
+"MTAwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMzETMBEG\n"
+"A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n"
+"hvcNAQEBBQADggEPADCCAQoCggEBAMwldpB5BngiFvXAg7aEyiie/QV2EcWtiHL8\n"
+"RgJDx7KKnQRfJMsuS+FggkbhUqsMgUdwbN1k0ev1LKMPgj0MK66X17YUhhB5uzsT\n"
+"gHeMCOFJ0mpiLx9e+pZo34knlTifBtc+ycsmWQ1z3rDI6SYOgxXG71uL0gRgykmm\n"
+"KPZpO/bLyCiR5Z2KYVc3rHQU3HTgOu5yLy6c+9C7v/U9AOEGM+iCK65TpjoWc4zd\n"
+"QQ4gOsC0p6Hpsk+QLjJg6VfLuQSSaGjlOCZgdbKfd/+RFO+uIEn8rUAVSNECMWEZ\n"
+"XriX7613t2Saer9fwRPvm2L7DWzgVGkWqQPabumDk3F2xmmFghcCAwEAAaNCMEAw\n"
+"DgYDVR0PAQH/BAQDAgEGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFI/wS3+o\n"
+"LkUkrk1Q+mOai97i3Ru8MA0GCSqGSIb3DQEBCwUAA4IBAQBLQNvAUKr+yAzv95ZU\n"
+"RUm7lgAJQayzE4aGKAczymvmdLm6AC2upArT9fHxD4q/c2dKg8dEe3jgr25sbwMp\n"
+"jjM5RcOO5LlXbKr8EpbsU8Yt5CRsuZRj+9xTaGdWPoO4zzUhw8lo/s7awlOqzJCK\n"
+"6fBdRoyV3XpYKBovHd7NADdBj+1EbddTKJd+82cEHhXXipa0095MJ6RMG3NzdvQX\n"
+"mcIfeg7jLQitChws/zyrVQ4PkX4268NXSb7hLi18YIvDQVETI53O9zJrlAGomecs\n"
+"Mx86OyXShkDOOyyGeMlhLxS67ttVb9+E7gUJTb0o2HLO02JQZR7rkpeDMdmztcpH\n"
+"WD9f\n"
+"-----END CERTIFICATE-----\n";
+
+
 
 /*
  * (optional) Client certificate here in PEM format.
@@ -26,8 +119,7 @@ const char* SSL_CA_PEM = NULL;
  * ...
  * "-----END CERTIFICATE-----\n";
  */
-const char* SSL_CLIENT_CERT_PEM = NULL;
-
+const char* SSL_CLIENT_CERT_PEM = "<< YOUR CLIENT CERT >>";
 
 /*
  * (optional) Client private key here in PEM format.
@@ -36,6 +128,5 @@ const char* SSL_CLIENT_CERT_PEM = NULL;
  * ...
  * "-----END RSA PRIVATE KEY-----\n";
  */
-const char* SSL_CLIENT_PRIVATE_KEY_PEM = NULL;
-
+const char* SSL_CLIENT_PRIVATE_KEY_PEM = "<< YOUR CLIENT PRIVATE KEY>>";
 #endif /* __MQTT_SERVER_SETTING_H__ */
--- main.cpp
+++ main.cpp
@@ -35,48 +35,52 @@
 #include "MQTT_server_setting.h"
 #include "mbed-trace/mbed_trace.h"
 #include "mbed_events.h"
+#include "NTPClient.h"
+#include "jwtgen.h"
 
-#define LED_ON 0
+#define MQTT_MAX_CONNECTIONS     5
+#define MQTT_MAX_PACKET_SIZE  1024
+
+#define TIME_JWT_EXP      (60*60*24)  // 24 hours (MAX)
+
+// LED on/off - This could be different among boards
+#define LED_ON  0    
 #define LED_OFF 1
 
+#include <string>
+
+/* Flag to be set when a message needs to be published, i.e. BUTTON is pushed. */
 static volatile bool isPublish = false;
+/* Flag to be set when received a message from the server. */
+static volatile bool isMessageArrived = false;
+/* Buffer size for a receiving message. */
+const int MESSAGE_BUFFER_SIZE = 1024;
+/* Buffer for a receiving message. */
+char messageBuffer[MESSAGE_BUFFER_SIZE];
 
-/*
- * Callback function called when a message arrived from server.
- */
-void messageArrived(MQTT::MessageData& md)
-{
-    /* TODO: Move printf to outside interrupt context. */
-    MQTT::Message &message = md.message;
-    printf("\r\n");
-    printf("! Message arrived: qos %d, retained %d, dup %d, packetid %d\r\n",
-            message.qos, message.retained, message.dup, message.id);
-    printf("! Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
-    printf("\r\n");
-}
-
-void btn1_rise_handler() {
-    isPublish = true;
-}
+// Function prototypes
+int getPassword(char *buf, size_t buf_size);
+void handleMqttMessage(MQTT::MessageData& md);
+void handleButtonRise();
 
 
 int main(int argc, char* argv[])
 {
     mbed_trace_init();
     
-    const float version = 0.8;
-    bool isSubscribed = false;
+    const float version = 0.1;
 
     NetworkInterface* network = NULL;
-    MQTTNetwork* mqttNetwork = NULL;
-    MQTT::Client<MQTTNetwork, Countdown>* mqttClient = NULL;
 
     DigitalOut led_red(LED_RED, LED_OFF);
-    DigitalOut led_green(LED_GREEN, LED_ON);
+    DigitalOut led_green(LED_GREEN, LED_OFF);
     DigitalOut led_blue(LED_BLUE, LED_OFF);
 
-    printf("HelloMQTT: version is %.2f\r\n", version);
+    printf("Mbed to Google IoT Cloud: version is %.2f\r\n", version);
     printf("\r\n");
+
+    // Turns on green LED to indicate processing initialization process
+    led_green = LED_ON;
 
     printf("Opening network interface...\r\n");
     {
@@ -89,6 +93,24 @@ int main(int argc, char* argv[])
     printf("Network interface opened successfully.\r\n");
     printf("\r\n");
 
+
+    /* NTP - Get the current time. Required to generate JWT. */
+    NTPClient ntp(network);
+    ntp.set_server("time.google.com", 123);
+    time_t now = ntp.get_timestamp();
+    set_time(now);
+
+    /* JWT - Set JWT as password.  */
+    const int buf_size = 1024;
+    char* password = new char[buf_size];
+    if(getPassword(password, buf_size) != 0) {
+        printf("ERROR: Failed to generate JWT.\r\n");
+        return -1;
+    }
+    printf("JWT:\r\n%s\r\n\r\n", password);
+
+    /* Establish a network connection. */
+    MQTTNetwork* mqttNetwork = NULL;
     printf("Connecting to host %s:%d ...\r\n", MQTT_SERVER_HOST_NAME, MQTT_SERVER_PORT);
     {
         mqttNetwork = new MQTTNetwork(network);
@@ -102,15 +124,22 @@ int main(int argc, char* argv[])
     printf("Connection established.\r\n");
     printf("\r\n");
 
-    printf("MQTT client is trying to connect the server ...\r\n");
+    // Generates MQTT cliend ID from user's parameter in MQTT_server_setting.h
+    std::string mqtt_client_id = 
+            std::string{"projects/"} + GOOGLE_PROJECT_ID + "/locations/"
+            + GOOGLE_REGION + "/registries/" + GOOGLE_REGISTRY + "/devices/" + GOOGLE_DEVICE_ID;
+
+    /* Establish a MQTT connection. */
+    MQTT::Client<MQTTNetwork, Countdown, MQTT_MAX_PACKET_SIZE, MQTT_MAX_CONNECTIONS>* mqttClient = NULL;
+    printf("MQTT client is connecting to the service ...\r\n");
     {
         MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-        data.MQTTVersion = 3;
-        data.clientID.cstring = (char *)MQTT_CLIENT_ID;
-        data.username.cstring = (char *)MQTT_USERNAME;
-        data.password.cstring = (char *)MQTT_PASSWORD;
+        data.MQTTVersion = 4; // 3 = 3.1 4 = 3.1.1
+        data.clientID.cstring = (char *)mqtt_client_id.c_str();
+        data.username.cstring = (char *)"ignored";
+        data.password.cstring = (char *)password;
 
-        mqttClient = new MQTT::Client<MQTTNetwork, Countdown>(*mqttNetwork);
+        mqttClient = new MQTT::Client<MQTTNetwork, Countdown, MQTT_MAX_PACKET_SIZE, MQTT_MAX_CONNECTIONS>(*mqttNetwork);
         int rc = mqttClient->connect(data);
         if (rc != MQTT::SUCCESS) {
             printf("ERROR: rc from MQTT connect is %d\r\n", rc);
@@ -120,35 +149,59 @@ int main(int argc, char* argv[])
     printf("Client connected.\r\n");
     printf("\r\n");
 
-    // Turn off the green LED
+    // Network initialization done. Turn off the green LED
     led_green = LED_OFF;
 
-    printf("Client is trying to subscribe a topic \"%s\".\r\n", MQTT_TOPIC_SUB);
+
+    // Generates topic names from user's setting in MQTT_server_setting.h
+    std::string mqtt_topic_pub = 
+            std::string{"/devices/"} + GOOGLE_DEVICE_ID + "/events";
+    std::string mqtt_topic_sub =
+            std::string{"/devices/"} + GOOGLE_DEVICE_ID + "/config";
+
+    /* Subscribe a topic. */
+    bool isSubscribed = false;
+    printf("Client is trying to subscribe a topic \"%s\".\r\n", mqtt_topic_sub.c_str());
     {
-        int rc = mqttClient->subscribe(MQTT_TOPIC_SUB, MQTT::QOS0, messageArrived);
+        int rc = mqttClient->subscribe(mqtt_topic_sub.c_str(), MQTT::QOS0, handleMqttMessage);
         if (rc != MQTT::SUCCESS) {
             printf("ERROR: rc from MQTT subscribe is %d\r\n", rc);
             return -1;
         }
         isSubscribed = true;
     }
-    printf("Client has subscribed a topic \"%s\".\r\n", MQTT_TOPIC_SUB);
+    printf("Client has subscribed a topic \"%s\".\r\n", mqtt_topic_sub.c_str());
     printf("\r\n");
 
-    // Enable button 1
+    // Enable button 1 for publishing a message.
     InterruptIn btn1 = InterruptIn(BUTTON1);
-    btn1.rise(btn1_rise_handler);
+    btn1.rise(handleButtonRise);
     
     printf("To send a packet, push the button 1 on your board.\r\n");
 
+    // Compose a message for the first time.
+    std::string initStr = std::string{"{\"cloudRegion\":\""} + GOOGLE_REGION + "\",\"deviceId\":\"" 
+                + GOOGLE_DEVICE_ID + ",\"registryId\":\"" + GOOGLE_REGISTRY + "\",\"hops\":1}"; 
+    char* buf = (char*)initStr.c_str();
 
+    /* Main loop */
     while(1) {
-        if(!mqttClient->isConnected()){
+        /* Client is disconnected. */
+        if(!mqttClient->isConnected()){        
             break;
         }
+        /* Waits a message and handles keepalive. */
         if(mqttClient->yield(100) != MQTT::SUCCESS) {
             break;
         }
+        /* Received a message. */
+        if(isMessageArrived) {
+            isMessageArrived = false;
+            printf("\r\nMessage arrived:\r\n%s\r\n", messageBuffer);
+            // Save the message to resend when the button is pushed.
+            buf = messageBuffer;
+        }
+        /* Button is pushed - publish a message. */
         if(isPublish) {
             isPublish = false;
             static unsigned int id = 0;
@@ -156,29 +209,25 @@ int main(int argc, char* argv[])
 
             count++;
 
-            // When sending a message, LED lights blue.
+            // When sending a message, blue LED lights.
             led_blue = LED_ON;
 
             MQTT::Message message;
             message.retained = false;
             message.dup = false;
 
-            const size_t buf_size = 100;
-            char *buf = new char[buf_size];
             message.payload = (void*)buf;
 
             message.qos = MQTT::QOS0;
             message.id = id++;
-            sprintf(buf, "%d", count);
-            message.payloadlen = strlen(buf)+1;
+            message.payloadlen = strlen(buf);
             // Publish a message.
-            printf("Publishing message.\r\n");
-            int rc = mqttClient->publish(MQTT_TOPIC_SUB, message);
+            printf("\r\nPublishing message to the topic %s:\r\n%s\r\n", mqtt_topic_pub.c_str(), buf);
+            int rc = mqttClient->publish(mqtt_topic_pub.c_str(), message);
             if(rc != MQTT::SUCCESS) {
                 printf("ERROR: rc from MQTT publish is %d\r\n", rc);
             }
             printf("Message published.\r\n");
-            delete[] buf;    
 
             led_blue = LED_OFF;
         }
@@ -188,8 +237,8 @@ int main(int argc, char* argv[])
 
     if(mqttClient) {
         if(isSubscribed) {
-            mqttClient->unsubscribe(MQTT_TOPIC_SUB);
-            mqttClient->setMessageHandler(MQTT_TOPIC_SUB, 0);
+            mqttClient->unsubscribe(mqtt_topic_sub.c_str());
+            mqttClient->setMessageHandler(mqtt_topic_sub.c_str(), 0);
         }
         if(mqttClient->isConnected()) 
             mqttClient->disconnect();
@@ -204,5 +253,47 @@ int main(int argc, char* argv[])
         // network is not created by new.
     }
 
-//    exit(0);
+    // Turn on the red LED when the program is done.
+    led_red = LED_ON;
+}
+
+/* 
+ * Creates a password string (JWT) to connect Google Cloud IoT Core. 
+ * @param buf Pointer to the buffer stores password string followed by terminate character, '\0'. 
+ * @param buf_size Size of the buffer in bytes.
+ * @return 0 when success, otherwise != 0
+ */
+int getPassword(char *buf, size_t buf_size) {
+    int ret = -1;
+    size_t len = 0;
+    time_t now = time(NULL);
+
+    JwtGenerator jwtgen;
+    jwtgen.setPrivateKey(SSL_CLIENT_PRIVATE_KEY_PEM);
+    if(jwtgen.getJwt(buf, buf_size, &len, GOOGLE_PROJECT_ID, now, now + TIME_JWT_EXP)
+             == JwtGenerator::SUCCESS) {
+        buf[len] = '\0';
+        ret = 0;
+    }
+    return ret;
+}
+
+/*
+ * Callback function called when a message arrived from server.
+ */
+void handleMqttMessage(MQTT::MessageData& md)
+{
+    // Copy payload to the buffer.
+    MQTT::Message &message = md.message;
+    memcpy(messageBuffer, message.payload, message.payloadlen);
+    messageBuffer[message.payloadlen] = '\0';
+
+    isMessageArrived = true;
+}
+
+/*
+ * Callback function called when button is pushed.
+ */
+void handleButtonRise() {
+    isPublish = true;
 }
